fea9939f4a489d6b204bb4621f9475d0baa0e4bd
DESKTOP-7AP6FNA\academia <kz748873@xj8.so-net.ne.jp>
2026-03-31 13:59:30 +0900

    ① HomeController と index.html 作成用プロンプト

---
+++ src/main/java/com/example/autoraceapp/controller/HomeController.java
@@ -0,0 +1,23 @@
+﻿package com.example.autoraceapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * ホーム画面を表示するためのコントローラです。
+ * 最初に "/" へアクセスしたとき、このクラスがテンプレートを返します。
+ */
+@Controller
+public class HomeController {
+
+	/**
+	 * トップページへの GET リクエストを受け取り、
+	 * src/main/resources/templates/home/index.html を表示します。
+	 *
+	 * @return 表示する Thymeleaf テンプレート名
+	 */
+	@GetMapping("/")
+	public String showHomePage() {
+		return "home/index";
+	}
+}
