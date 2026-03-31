74f015f256338d4fc6feb6098256e083ff0306cb
DESKTOP-7AP6FNA\academia <kz748873@xj8.so-net.ne.jp>
2026-03-31 13:51:21 +0900

    スペルチェック補正

--- src/main/java/com/example/autoraceapp/AutoraceappApplication.java
+++ src/main/java/com/example/autoraceapp/AutoraceappApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AutoraceappApplication {
+public class AutoRaceAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AutoraceappApplication.class, args);
+		SpringApplication.run(AutoRaceAppApplication.class, args);
 	}
 
 }
