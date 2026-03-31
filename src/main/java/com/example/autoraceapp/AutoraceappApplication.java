49d641ede246bd32ee137eef9c64f69199c6a1da
DESKTOP-7AP6FNA\academia <kz748873@xj8.so-net.ne.jp>
2026-03-31 13:39:26 +0900

    application.properties を設定する

---
+++ src/main/java/com/example/autoraceapp/AutoraceappApplication.java
@@ -0,0 +1,13 @@
+package com.example.autoraceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoraceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoraceappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/autoraceapp/AutoraceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autoraceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoraceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
